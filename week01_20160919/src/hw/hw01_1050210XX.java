fab3c2b8e84154c154fa92e3fc5b1c97b33f7761
a298582548 <105021010@live.asia.edu.tw>
2016-09-19 16:53:53 +0800

    微調

--- week01_20160919/src/hw/hw01_1050210XX.java
+++ week01_20160919/src/hw/hw01_1050210XX.java
@@ -1,4 +1,9 @@
 package hw;
+/*
+ * Topic: 假設我們要用二進制來表示 n 種狀態，請寫一個程式來計算記錄這 n 種狀態需要多少位元 (例如: n = 3 則需要 2bits)  (log(n)/log(2)
+ * Date: 2016/09/19
+ * Author: 105021010 邱信喆
+ */
 
 public class hw01_1050210XX {
 
